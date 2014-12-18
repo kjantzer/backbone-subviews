5743f3ec0753496e549671a10ff4e249b76db9da
kevin.jantzer <kevin.jantzer@blackstoneaudio.com>
2014-12-18 13:11:56 -0800

    Uninitialized subviews can be set; they will auto initalize the first time accessed

--- backbone.subviews.js
+++ backbone.subviews.js
@@ -11,6 +11,18 @@
 
 _.extend(Backbone.View.prototype, {
 	
+
+/*
+	Subview - adds or retrieves subview
+
+	When adding a subview, you can choose to pass in a view instance
+	or just the class to use when initialized view
+
+		var myView = Backbone.View.extend();
+
+		this.subview('my-view', new myView());	// initialized right away
+		this.subview('my-view', myView;			// only inialized the first time it is accessed
+*/
 	subview: function(viewName, view){
 
 		this.__subviews = this.__subviews || {};
@@ -22,14 +34,27 @@ _.extend(Backbone.View.prototype, {
 
 			// set the new view
 			this.__subviews[viewName] = view;
-			view.viewName = viewName;
-			view.parentView = this;
+			
+			if( typeof view === 'object' ) // only setup view if it has been intialized
+				this._setupSubview(viewName, view);
+
 			return view;
 		
 		// return the requested view
 		}else{
-			return this.__subviews[viewName];
+			var view = this.__subviews[viewName];
+
+			if( typeof view === 'function' )
+				view = this.__subviews[viewName] = this._setupSubview(viewName, new view() );
+
+			return view;
 		}
+	},
+
+	_setupSubview: function(viewName, view){
+		view.viewName = viewName;
+		view.parentView = this;
+		return view;
 	},
 
 	_removeSubview: function(view){
@@ -70,9 +95,16 @@ _.extend(Backbone.View.prototype, {
 	},
 
 	cleanupSubviews: function(andClear){
+
 		this.view && this.view.cleanup();		// main child view if it exists
 		this.editor && this.editor.cleanup();	// model editor if it exists
-		_.invoke(this.__subviews, 'cleanup');	// all subviews
+
+		// all subviews
+		_.each(this.__subviews, function(view){
+			if( view.cleanup ) // test since view may not be initialized
+				view.cleanup();
+		});
+
 		andClear && this.clearSubviews();		// clear subviews if requested
 	},
 
